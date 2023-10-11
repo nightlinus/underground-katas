bda69bb99f4dfe61d32c1e9e9499d61f37eab09b
ddfrolov <ddfrolov@avito.ru>
2023-10-11 10:53:21 +0400

    Add Test_line_number_devisable_by_5_print_Buzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -2,12 +2,22 @@ package fizzbuzz
 
 import "strconv"
 
-func FizzBuzz() []string {
-	lines := make([]string, 100)
+type FizzBuzzLines []string
+
+func (l FizzBuzzLines) Line(num int64) string {
+	return l[num-1]
+}
+
+func FizzBuzz() FizzBuzzLines {
+	lines := make(FizzBuzzLines, 100)
 	for i, _ := range lines {
 		lines[i] = strconv.Itoa(i + 1)
 		if (i+1)%3 == 0 {
 			lines[i] = "Fizz"
+		}
+
+		if (i+1)%5 == 0 {
+			lines[i] = "Buzz"
 		}
 	}
 
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -15,15 +15,32 @@ func Test_FizzBuzz_print_100_lines(t *testing.T) {
 func Test_FizzBuzz_each_line_prints_its_number(t *testing.T) {
 	res := fizzbuzz.FizzBuzz()
 
-	assert.Equal(t, res[0], "1")
-	assert.Equal(t, res[1], "2")
-	assert.Equal(t, res[99], "100")
+	assert.Equal(t, "1", res.Line(1))
+	assert.Equal(t, "2", res.Line(2))
+	assert.Equal(t, "98", res.Line(98))
 }
 
 func Test_line_number_devisable_by_3_print_Fizz(t *testing.T) {
 	lines := fizzbuzz.FizzBuzz()
 
-	assert.Equal(t, lines[2], "Fizz")
-	assert.Equal(t, lines[5], "Fizz")
-	assert.Equal(t, lines[98], "Fizz")
+	assert.Equal(t, "Fizz", lines.Line(3))
+	assert.Equal(t, "Fizz", lines.Line(6))
+	assert.Equal(t, "Fizz", lines.Line(99))
+}
+
+func Test_line_number_devisable_by_5_print_Buzz(t *testing.T) {
+	lines := fizzbuzz.FizzBuzz()
+
+	assert.Equal(t, "Buzz", lines.Line(5))
+	assert.Equal(t, "Buzz", lines.Line(10))
+	assert.Equal(t, "Buzz", lines.Line(100))
+}
+
+func Test_line_number_devisable_by_5_and_3_print_FizzBuzz(t *testing.T) {
+	lines := fizzbuzz.FizzBuzz()
+
+	assert.Equal(t, "FizzBuzz", lines.Line(15))
+	assert.Equal(t, "FizzBuzz", lines.Line(45))
+	assert.Equal(t, "FizzBuzz", lines.Line(60))
+	assert.Equal(t, "FizzBuzz", lines.Line(90))
 }
