61796b4b43ec1c09b1ebe2859c6d24d9e4fa0fcb
Vyacheslav Osipov <vyosipov@avito.ru>
2024-01-29 13:30:56 +0500

    todo added

--- bank_ocr/bank_ocr_test.go
+++ bank_ocr/bank_ocr_test.go
@@ -289,6 +289,9 @@ func Test_parsed_accounts_output_format(t *testing.T) {
 		"111111111\n111111111\n111111111\n", out)
 }
 
+// TODO:
+//  1. Изменить тестовые данные
+//     2.(opt) Добавить генераторы для валидных номеров
 func Test_parsed_accounts_output_format_with_ill(t *testing.T) {
 	out := bank_ocr.OutputFormat(`                           
   |  |  |  |  |  |  |  |  |
